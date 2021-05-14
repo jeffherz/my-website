cfe21474c694bbb3f3b7a8e948d3191bdb9295c9
Jeff Herz (JJ) <66926205+jeffherz@users.noreply.github.com>
2021-05-13 22:50:54 -0400

    Update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,7 +6,9 @@
     I can actually do this.  So here I am, making it in the world creating my personal website... Hell yeah!!!
 </p>
 <a href="linkedin.com/in/jeffreyherz/">Here is my LinkedIne profile</a>
-
+<h1>Past Experience</h1>
+<p>From November of 2020 to March 2021, I got the opportunity to be a academic fellow for a Tech Elevator cohort where I would help the students with their assignments, answer any questions that they might have, and assist the instructor with whatever they needed.  It was great getting to work with the students and change strategy with the different personalities and learning styles.
+</p>
 </template>
 
 <script>
